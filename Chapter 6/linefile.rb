2975d9edb9537baf9206d87d398cc2b089f99f34
Jeffrey Siv <Thinksiv@gmail.com>
2015-03-08 05:45:42 -0400

    adding new files

---
+++ Chapter 6/linefile.rb
@@ -0,0 +1,15 @@
+def line_from_file(filename, substring)
+  fh = File.open(filename)
+  begin
+    line = fh.gets
+    raise ArgumentError unless line.include?(substring)
+  rescue ArgumentError
+    puts "Invalid line!"
+    raise
+  ensure
+    fh.close
+  end
+  return line
+end
+
+# 6.4.5 The ensure clause
